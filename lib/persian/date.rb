589790027266703c22186b87450d258ea48ccd69
dariush abbasi <dariushem@yahoo.com>
2016-08-16 19:10:25 +0430

    update jalali date class with some bug fixes

--- lib/persian/date.rb
+++ lib/persian/date.rb
@@ -5,96 +5,86 @@ module Persian
   # Persian count class
   # Basic  counters for persian chars, texts, sentences and paragraphs
   class Date
-
-    def toJalali(gy, gm, gd)
+    def to_jalali(gy, gm, gd)
       d2j(g2d(gy, gm, gd))
     end
 
-    def toGregorian(jy, jm, jd)
+    def to_gregorian(jy, jm, jd)
       d2g(j2d(jy, jm, jd))
     end
 
-    def isValidJalaaliDate(jy, jm, jd)
-      jy >= -61 && jy<=3177 && jm >= 1 && jm <= 12 && jd >= 1 && jd <= jalaaliMonthLength(jy, jm)
+    def valid_jalali_date?(jy, jm, jd)
+      jy >= -61 && jy <= 3177 && jm >= 1 && jm <= 12 && jd >= 1 && jd <= jalali_month_length(jy, jm)
     end
 
-    def isLeapJalaaliYear(jy)
-      jalCal(jy)[:leap] == 0
+    def leap_jalali_year?(jy)
+      jal_cal(jy)[:leap].zero?
     end
 
-    def jalaaliMonthLength(jy, jm)
+    def jalali_month_length(jy, jm)
       return 31 if jm <= 6
       return 30 if jm <= 11
-      return 30 if isLeapJalaaliYear(jy)
-      return 29
+      return 30 if leap_jalali_year?(jy)
+      29
     end
 
-    def jalCal(jy)
+    def jal_cal(jy)
       breaks = [-61, 9, 38, 199, 426, 686, 756, 818, 1111, 1181, 1210,
                 1635, 2060, 2097, 2192, 2262, 2324, 2394, 2456, 3178]
 
-      bl = breaks.length
+      bl = breaks.size
       gy = jy + 621
-      leapJ = -14
+      leap_j = -14
       jp = breaks[0]
 
-      raise Error if jy < jp || jy >= breaks[bl- 1]
+      raise Error if jy < jp || jy >= breaks[bl - 1]
 
-      jump = 0
+      jump = nil
 
-      1.upto(bl) do |i|
-        if bl = i
-          break
-        end
+      1.upto(bl - 1) do |i|
         jm = breaks[i]
         jump = jm - jp
 
-        if jy < jm
-          break
-        end
+        break if jy < jm
 
-        leapJ = leapJ + div(jump, 33) * 8 + div(mod(jump, 33), 4)
+        leap_j = leap_j + div(jump, 33) * 8 + div(mod(jump, 33), 4)
         jp = jm
       end
 
       n = jy - jp
-      leapJ = leapJ + div(n, 33) * 8 + div(mod(n, 33), 4)
 
-      if(mod(jump, 33) == 4 && jump - n == 4)
-        leapJ += 1
-      end
+      leap_j = leap_j + div(n, 33) * 8 + div(mod(n, 33), 4)
 
-      leapG = div(gy, 4) - div((div(gy, 100) + 1) * 3, 4) - 150
-      march = 20 + leapJ - leapG
+      leap_j += 1 if mod(jump, 33) == 4 && jump - n == 4
 
-      if(jump - n < 6)
-        n = n - jump + div(jump + 4, 33) * 33
-      end
+      leap_g = div(gy, 4) - div((div(gy, 100) + 1) * 3, 4) - 150
+
+      march = 20 + leap_j - leap_g
+
+      n = n - jump + div(jump + 4, 33) * 33 if jump - n < 6
 
       leap = mod(mod(n + 1, 33) - 1, 4)
 
-      if (leap == -1)
-        leap = 4
-      end
+      leap = 4 if leap == -1
 
-      return  { leap: leap,
-                gy: gy,
-                march: march }
+      { leap: leap,
+        gy: gy,
+        march: march }
     end
 
     def j2d(jy, jm, jd)
-      r = jalCal(jy)
+      r = jal_cal(jy)
       g2d(r[:gy], 3, r[:march]) + (jm - 1) * 31 - div(jm, 7) * (jm - 7) + jd - 1
     end
 
     def d2j(jdn)
       gy = d2g(jdn)[:gy]
       jy = gy - 621
-      r = jalCal(jy)
+      r = jal_cal(jy)
       jdn1f = g2d(gy, 3, r[:march])
 
       k = jdn - jdn1f
-      if(k >= 0) then
+      if k >= 0
         (
         if k <= 185
           jm = 1 + div(k, 31)
@@ -111,47 +101,50 @@ module Persian
       else
         jy -= 1
         k += 179
-        if(r[:leap] == 1)
-          k +=1
-        end
+        k += 1 if r[:leap] == 1
       end
       jm = 7 + div(k, 30)
       jd = mod(k, 30) + 1
 
-      return {
+      {
         jy: jy,
         jm: jm,
         jd: jd
       }
-
     end
 
     def g2d(gy, gm, gd)
-      d = div((gy + div(gm - 8, 6) + 100100) * 1461, 4) + div(153 * mod(gm + 9, 12) + 2, 5) + gd - 34840408
-      d - div(div(gy + 100100 + div(gm - 8, 6), 100) * 3, 4) + 752
+      d = div((gy + div(gm - 8, 6) + 100_100) * 1_461, 4) + div(153 * mod(gm + 9, 12) + 2, 5) + gd - 34_840_408
+      d - div(div(gy + 100_100 + div(gm - 8, 6), 100) * 3, 4) + 752
     end
 
     def d2g(jdn)
-      j = 4 * jdn + 139361631
-      j = j + div(div(4 * jdn + 183187720, 146097) * 3, 4) * 4 - 3908
-      i = div(mod(j, 1461), 4) * 5 + 308
+      j = 4 * jdn + 139_361_631
+      j = j + div(div(4 * jdn + 183_187_720, 146_097) * 3, 4) * 4 - 3_908
+      i = div(mod(j, 1_461), 4) * 5 + 308
       gd = div(mod(i, 153), 5) + 1
       gm = mod(div(i, 153), 12) + 1
-      gy = div(j, 1461) - 100100 + div(8 - gm, 6)
+      gy = div(j, 1_461) - 100_100 + div(8 - gm, 6)
 
-      return  { gy: gy,
-                gm: gm,
-                gd: gd
-              }
+      { gy: gy,
+        gm: gm,
+        gd: gd }
     end
 
     def div(a, b)
-      return Integer(a / b)
+      tilde(a / b.to_f)
     end
 
     def mod(a, b)
-      return a - Integer(a / b) * b
+      a - tilde(a / b.to_f) * b
     end
 
+    def tilde(num)
+      if num < 0
+        num.ceil
+      else
+        num.floor
+      end
+    end
   end
 end
