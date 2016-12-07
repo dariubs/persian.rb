5e46dd389571c92229d2520d4140b78f65000ddd
Dariush Abbasi <dariushem@yahoo.com>
2016-12-07 04:24:51 +0330

    complete Honoyms

--- lib/persian/list/homonyms.rb
+++ lib/persian/list/homonyms.rb
@@ -31,6 +31,7 @@ module Persian
       QAF
     ].freeze
 
+    # List of all Homonyms classified in a hash
     ALL = {
       T: T,
       S: S,
@@ -38,5 +39,21 @@ module Persian
       Z: Z,
       GH: GH
     }.freeze
+
+    # List of all Homonyms bulk in array
+    ALL_a = [
+      T, S, H, Z, GH
+    ].flatten.freeze
+
+    # Hash reverse list of Homonyms
+    temp = {}
+
+    ALL.each do |key, value|
+      value.each do |i|
+        temp[i.to_s] = key
+      end
+    end
+
+    ALL_r = temp.freeze
   end
 end
